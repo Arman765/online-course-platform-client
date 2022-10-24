94bed8b0123c51805ccf6dcad3c9b7001f6364cc
Arman765 <friend.of.laden765@gmail.com>
2022-10-25 05:59:07 +0600

    home

--- src/Pages/Home/Home/Home.js
+++ src/Pages/Home/Home/Home.js
@@ -1,10 +1,33 @@
 import React from "react";
+import Container from "react-bootstrap/Container";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
+import onlineEdu from "../../../assets/home/home.jpg";
+import Button from "react-bootstrap/Button";
 
 const Home = () => {
   return (
-    <div>
-      <h3>This is home</h3>
-    </div>
+    <Container>
+      <Row>
+        <Col>
+          <div className="">
+            <h1 className="align-text-bottom fs-1">
+              Online <span className="text-primary">Learning</span>
+            </h1>
+            <div className="mb-2">
+              <Button variant="primary" size="lg">
+                Courses
+              </Button>
+            </div>
+          </div>
+        </Col>
+        <Col>
+          <div>
+            <img className="img-fluid" src={onlineEdu} alt="onlineEdu"></img>
+          </div>
+        </Col>
+      </Row>
+    </Container>
   );
 };
 
