66fef704306c04d2a32dabd60e794874412b4f9d
Arman765 <friend.of.laden765@gmail.com>
2022-10-25 02:34:20 +0600

    starting

---
+++ src/firebase/firebase.init.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyAzKV2t73dDPDa6voCKZTT4-w9f_-tiIHE",
+  authDomain: "freedom-course.firebaseapp.com",
+  projectId: "freedom-course",
+  storageBucket: "freedom-course.appspot.com",
+  messagingSenderId: "116700560923",
+  appId: "1:116700560923:web:0b4a22e5ac15e8bc8882be",
+};
+
+// Initialize Firebase
+export const app = initializeApp(firebaseConfig);
